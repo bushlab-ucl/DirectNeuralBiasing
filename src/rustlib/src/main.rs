e3f056bccc91e114bc2ac55b9b0d602704d9bda7
dnhmphrs <dan@aufbau.io>
2023-11-02 10:32:27 +0000

    rebuild rust code

--- src/rustlib/src/main.rs
+++ src/rustlib/src/main.rs
@@ -1,16 +1,14 @@
 mod filters;
+#[cfg(not(feature = "python-extension"))]
+mod local;
 
 #[cfg(not(feature = "python-extension"))]
-mod client;
-#[cfg(not(feature = "python-extension"))]
-mod server;
-
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         match args[1].as_str() {
-            "client" => client::run().unwrap(),
-            "server" => server::run().unwrap(),
+            "client" => local::client::run().unwrap(),
+            "server" => local::server::run().unwrap(),
             _ => println!("Invalid argument, please use 'client' or 'server'"),
         }
     } else {
---
+++ src/rustlib/src/pythonlib/py03.rs
@@ -0,0 +1,78 @@
+use crate::filters::bandpass::BandPassFilter;
+
+// #[cfg(feature = "python-extension")]
+use pyo3::prelude::*;
+// #[cfg(feature = "python-extension")]
+use pyo3::{wrap_pyfunction, wrap_pymodule};
+
+// #[cfg(feature = "python-extension")]
+#[pymodule]
+#[pyo3(name = "dnb")]
+fn dnb(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pymodule!(filters_module))?;
+    Ok(())
+}
+
+// #[cfg(feature = "python-extension")]
+#[pymodule]
+#[pyo3(name = "filters")]
+fn filters_module(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(butterworth_filter, m)?)?;
+    m.add_function(wrap_pyfunction!(biquad_filter, m)?)?;
+    m.add_function(wrap_pyfunction!(chebyshev_filter, m)?)?;
+    Ok(())
+}
+
+// #[cfg(feature = "python-extension")]
+#[pyfunction]
+#[pyo3(name = "butterworth")]
+fn butterworth_filter(f0: f64, fs: f64, signal: Vec<f64>) -> PyResult<Vec<f64>> {
+    let mut butterworth = BandPassFilter::butterworth(f0, fs);
+
+    // Add your own code to generate or fetch the input data
+    // let input_data = vec![0.0, 1.0, 2.0]; // Placeholder
+
+    let mut filered_signal = vec![];
+    for value in signal {
+        let filtered = butterworth.filter(value);
+        filered_signal.push(filtered);
+    }
+
+    Ok(filered_signal)
+}
+
+// #[cfg(feature = "python-extension")]
+#[pyfunction]
+#[pyo3(name = "biquad")]
+fn biquad_filter(f0: f64, fs: f64, q: f64, signal: Vec<f64>) -> PyResult<Vec<f64>> {
+    let mut biquad = BandPassFilter::biquad(f0, fs, q);
+
+    // Add your own code to generate or fetch the input data
+    // let input_data = vec![0.0, 1.0, 2.0]; // Placeholder
+
+    let mut filered_signal = vec![];
+    for value in signal {
+        let filtered = biquad.filter(value);
+        filered_signal.push(filtered);
+    }
+
+    Ok(filered_signal)
+}
+
+// #[cfg(feature = "python-extension")]
+#[pyfunction]
+#[pyo3(name = "chebyshev")]
+fn chebyshev_filter(f0: f64, fs: f64, q: f64, signal: Vec<f64>) -> PyResult<Vec<f64>> {
+    let mut chebyshev = BandPassFilter::chebyshev(f0, fs, q);
+
+    // Add your own code to generate or fetch the input data
+    // let input_data = vec![0.0, 1.0, 2.0]; // Placeholder
+
+    let mut filered_signal = vec![];
+    for value in signal {
+        let filtered = chebyshev.filter(value);
+        filered_signal.push(filtered);
+    }
+
+    Ok(filered_signal)
+}
