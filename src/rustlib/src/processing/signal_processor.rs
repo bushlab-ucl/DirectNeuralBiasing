83cbaa181e1d54f052bda4d0e33181fa1b2bf578
dnhmphrs <dan@aufbau.io>
2024-05-27 07:44:38 +0100

    hotfix

--- src/rustlib/src/processing/signal_processor.rs
+++ src/rustlib/src/processing/signal_processor.rs
@@ -32,8 +32,8 @@ pub struct SignalProcessor {
 impl SignalProcessor {
     pub fn new(config: SignalProcessorConfig) -> Self {
         SignalProcessor {
-            index: 0,
-            sample_count: 0,
+            index: -1,        // Start at -1 so that the first index is 0
+            sample_count: -1, // Start at -1 so that the first count is 0
             filters: HashMap::new(),
             detectors: HashMap::new(),
             triggers: HashMap::new(),
@@ -62,6 +62,9 @@ impl SignalProcessor {
         let mut output = Vec::new();
 
         for sample in raw_samples {
+            self.index += 1;
+            self.sample_count += 1;
+
             if self.sample_count % self.config.downsample_rate != 0 {
                 continue;
             }
@@ -88,9 +91,6 @@ impl SignalProcessor {
             }
 
             output.push(self.results.clone());
-
-            self.index += 1;
-            self.sample_count += 1;
         }
 
         output
